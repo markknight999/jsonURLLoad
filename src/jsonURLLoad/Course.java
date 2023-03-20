301aba48e2c96320ec518c8aae724fc157a0060a
Mark <markknight_999@outlook.com>
2023-03-19 22:54:19 -0500

    Spring Framework Assignment I could not get the mapping for the courses to map. I tried for 8 days straight and could not get them to work. I gave up and manually added the values just so I could get the rest of the assignment to work.

---
+++ src/jsonURLLoad/Course.java
@@ -0,0 +1,78 @@
+package jsonURLLoad;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Course {
+
+    private int id;
+    private String courseNo;
+    private String courseName;
+    private String grade;
+    private String creditHours;
+    private List<Student> students;
+
+    public Course(int id, String courseNo, String grade, String creditHours) {
+        this.id = id;
+        this.courseNo = courseNo;
+        //this.courseName = courseName;
+        this.grade = grade;
+        this.creditHours = creditHours;
+        //this.students = new ArrayList<Student>();
+    }
+
+    public Course(String courseNo, String grade, String creditHours) {
+        this.courseNo = courseNo;
+        this.grade = grade;
+        this.creditHours = creditHours;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCourseNo() { return courseNo; }
+
+    public void setCourseNo(String courseNo) { this.courseNo = courseNo; }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getGrade() { return grade; }
+
+    public void setGrade(String grade) { this.grade = grade; }
+
+    public String getCreditHours() { return creditHours; }
+
+    public void setCreditHours(String creditHours) { this.creditHours = creditHours; }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", courseNo='" + courseNo + '\'' +
+                ", courseName='" + courseName + '\'' +
+                ", grade = '" + grade + '\'' +
+                ", creditHours = '" + creditHours + '\'' +
+                ", students = " + students +
+                '}';
+    }
+
+}
---
+++ src/jsonURLLoad/Student.java
@@ -0,0 +1,82 @@
+package jsonURLLoad;
+
+import java.util.ArrayList;
+import java.util.List;
+
+    public class Student {
+
+        private int id;
+        private String first_name;
+        private String email;
+        private String gender;
+        private List<Course> courses;
+
+        public Student(int id, String first_name, String email, String gender, ArrayList<Course> courses) {
+            this.id = id;
+            this.first_name = first_name;
+            this.email = email;
+            this.gender = gender;
+            this.courses = new ArrayList<Course>();
+        }
+
+        public Student(int id, String first_name, String email, String gender) {
+            this.id = id;
+            this.first_name = first_name;
+            this.email = email;
+            this.gender = gender;
+        }
+        
+        public Student() {
+        	
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getFirst_name() {
+            return first_name;
+        }
+
+        public void setFirst_name(String first_name) {
+            this.first_name = first_name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) { this.email = email; }
+
+        public String getGender() {
+            return gender;
+        }
+
+        public void setGender(String gender) {
+            this.gender = gender;
+        }
+
+        public List<Course> getCourses() {
+            return courses;
+        }
+
+        public void setCourses(List<Course> courses) {
+            this.courses = courses;
+        }
+
+        @Override
+        public String toString() {
+            return "Student{" +
+                    "id=" + id +
+                    ", name='" + first_name + '\'' +
+                    ", email='" + email + '\'' +
+                    ", gender='" + gender + '\'' +
+                    ", courses=" + courses +
+                    '}';
+        }
+
+}
