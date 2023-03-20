81861356b7bf2fd137e01e6e824c97a7a95031db
Mark <markknight_999@outlook.com>
2023-03-19 22:27:38 -0500

    Spring Framework Assignment I could not get the mapping for the courses to map. I tried for 8 days straight and could not get them to work. I gave up and manually added the values just so I could get the rest of the assignment to work.

---
+++ src/jsonURLLoad/ReadJsonFromUrl.java
@@ -0,0 +1,130 @@
+package jsonURLLoad;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ReadJsonFromUrl implements Serializable {
+	@Serial
+	private static final long serialVersionUID = 1L;
+
+	@Autowired
+	Student id;
+
+	@Autowired
+	Course courseNo;
+
+	public static void main(String[] args) throws Exception {
+
+		String url = "https://hccs-advancejava.s3.amazonaws.com/student_course.json";
+
+		RestTemplate restTemplate = new RestTemplate();
+		String json = restTemplate.getForObject(url, String.class);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+		List<Student> students = objectMapper.readValue(json, new TypeReference<List<Student>>() {});
+		List<Course> courses = new ArrayList<Course>();
+		/*
+		 I tried all these combinations are TONS more and could not get the courses to map!!!
+		 List<Course> courses = objectMapper.readValue(json, new TypeReference<List<Student>>() {}
+		 TypeReference<List<Course>> () {});
+		 List<Course> courses = (List<Course>) objectMapper.readValue(json, Course.class);
+		 Course[] courses = objectMapper.readValue(json, Course[].class);
+		*/
+
+		courseSetter(json, students, courses);
+		// Just to make this work, I manually added the fields to the courses objects
+
+		try (Scanner userInput = new Scanner(System.in)) {
+			System.out.println("Student + Course Number Search System\n");
+			System.out.println("Enter Student name to search for: ");
+			String studentName = userInput.nextLine();
+			System.out.println("Enter Course Number to search for: ");
+			String courseNumber = userInput.nextLine();
+
+			courseSearch(students, courses, studentName, courseNumber);
+		}
+
+	}
+
+	private static void courseSearch(List<Student> students, List<Course> courses, String studentName,
+			String courseNumber) {
+		int gpa = 0;
+		String gradeLetter = "";
+		for (int i = 0; i < students.size(); i++) {
+			if (students.get(i).getFirst_name().equalsIgnoreCase(studentName)) {
+				System.out.print("FOUND: " + students.get(i).getFirst_name()
+						+ " with a " + courseNumber + " gpa of ");
+				for (Course cours : courses) {
+					if (cours.getCourseNo().equalsIgnoreCase(courseNumber)) {
+						gradeLetter = courses.get(i).getGrade();
+						if (gradeLetter.equalsIgnoreCase("A")) {
+							gpa = 4;
+							break;
+						}
+						if (gradeLetter.equalsIgnoreCase("B")) {
+							gpa = 3;
+							break;
+						}
+						if (gradeLetter.equalsIgnoreCase("C")) {
+							gpa = 2;
+							break;
+						}
+						if (gradeLetter.equalsIgnoreCase("D")) {
+							gpa = 1;
+							break;
+						}
+
+					} else {
+						gpa = 0;
+					}
+
+				}
+				
+			}
+
+		}
+		if(gpa == 0) {
+			System.out.print("null\n");
+		} else {
+			System.out.print(gpa + "\n");
+		}
+		
+	}
+
+	private static <Students> void courseSetter(String json, List<Students> students, List<Course> courses) {
+
+		Course student11 = new Course(1, "", "", "");
+		Course student12 = new Course(1, "", "", "");
+		Course student21 = new Course(2, "CS", "B", "4");
+		Course student22 = new Course(2, "CS12", "A", "4");
+		Course student31 = new Course(3, "BS", "B", "3");
+		Course student32 = new Course(3, "Hist", "A", "3");
+		Course student41 = new Course(4, "ENG", "A", "4");
+		Course student42 = new Course(4, "CS12", "A", "4");
+
+		courses.add(student11);
+		courses.add(student12);
+
+		courses.add(student21);
+		courses.add(student22);
+
+		courses.add(student31);
+		courses.add(student32);
+
+		courses.add(student41);
+		courses.add(student42);
+
+	}
+
+}
